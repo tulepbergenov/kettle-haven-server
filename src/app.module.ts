f3a309ff2e372457c0dadd3c4478bc87111d7e55
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-05-02 01:14:58 +0600

    feat: make connection db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeConfigService } from './config/sequlizeConfig.service';
+import { databaseConfig } from './config/configuration';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+  findOne(filter: {
+    where: { id?: string; username?: string; email?: string };
+  }): Promise<User> {
+    return this.userModel.findOne({ ...filter });
+  }
+  async create(createUserDto): Promise<User | { warningMessage: string }> {}
+}
