34559a2f442686672c56e80dd7768cfd486fef4c
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-05-02 15:44:28 +0600

    fix: localauthguard

--- src/auth/local.auth.guard.ts
+++ src/auth/local.auth.guard.ts
@@ -1,9 +1,9 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-  async canActive(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const result = (await super.canActivate(context)) as boolean;
     const request = context.switchToHttp().getRequest();
 
