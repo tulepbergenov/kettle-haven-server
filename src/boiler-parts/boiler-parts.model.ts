870273bc3a5a045418d6275b1d1ca35f2dcaa360
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-05-03 13:09:48 +0600

    feat: make boiler-parts model

---
+++ src/boiler-parts/boiler-parts.model.ts
@@ -0,0 +1,40 @@
+import { Column, Model, Table } from 'sequelize-typescript';
+
+@Table
+export class BoilerParts extends Model {
+  @Column
+  boiler_manufacturer: string;
+
+  @Column({ defaultValue: 0 })
+  price: number;
+
+  @Column
+  parts_manufacturer: string;
+
+  @Column
+  vendor_code: number;
+
+  @Column
+  name: string;
+
+  @Column
+  description: string;
+
+  @Column
+  images: string;
+
+  @Column({ defaultValue: 0 })
+  in_stock: number;
+
+  @Column({ defaultValue: false })
+  bestsellers: boolean;
+
+  @Column({ defaultValue: false })
+  new: boolean;
+
+  @Column
+  popularity: number;
+
+  @Column
+  compatibility: string;
+}
