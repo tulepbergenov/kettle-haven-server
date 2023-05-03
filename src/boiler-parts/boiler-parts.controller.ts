b78eb3f1a5012a8209ccc9ba0fb2da4478f02005
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-05-03 14:55:15 +0600

    feat: update boiler-parts

--- src/boiler-parts/boiler-parts.controller.ts
+++ src/boiler-parts/boiler-parts.controller.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { BoilerPartsService } from './boiler-parts.service';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
+import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
+import { PaginateAndFilterResponse } from './types';
+import { FindOneResponse } from './types';
+import { GetNewResponse } from './types';
+import { GetBestsellersResponse } from './types';
+import { SearchResponse } from './types';
+import { SearchRequest } from './types';
+import { GetByNameResponse } from './types';
+import { GetByNameRequest } from './types';
 
 @Controller('boiler-parts')
-export class BoilerPartsController {}
+export class BoilerPartsController {
+  constructor(private readonly boilerPartsService: BoilerPartsService) {}
+
+  @ApiOkResponse({ type: PaginateAndFilterResponse })
+  @UseGuards(AuthenticatedGuard)
+  @Get()
+  paginateAndFilter(@Query() query) {
+    return this.boilerPartsService.paginateAndFilter(query);
+  }
+
+  @ApiOkResponse({ type: FindOneResponse })
+  @UseGuards(AuthenticatedGuard)
+  @Get('find/:id')
+  getOne(@Param('id') id: string) {
+    return this.boilerPartsService.findOne(id);
+  }
+
+  @ApiOkResponse({ type: GetBestsellersResponse })
+  @UseGuards(AuthenticatedGuard)
+  @Get('bestsellers')
+  getBestseller() {
+    return this.boilerPartsService.bestsellers();
+  }
+
+  @ApiOkResponse({ type: GetNewResponse })
+  @UseGuards(AuthenticatedGuard)
+  @Get('new')
+  getNew() {
+    return this.boilerPartsService.new();
+  }
+
+  @ApiOkResponse({ type: SearchResponse })
+  @ApiBody({ type: SearchRequest })
+  @UseGuards(AuthenticatedGuard)
+  @Post('search')
+  search(@Body() { search }: { search: string }) {
+    return this.boilerPartsService.searchByString(search);
+  }
+
+  @ApiOkResponse({ type: GetByNameResponse })
+  @ApiBody({ type: GetByNameRequest })
+  @UseGuards(AuthenticatedGuard)
+  @Post('name')
+  getByName(@Body() { name }: { name: string }) {
+    return this.boilerPartsService.findOneByName(name);
+  }
+}
--- src/boiler-parts/boiler-parts.model.ts
+++ src/boiler-parts/boiler-parts.model.ts
@@ -27,7 +27,7 @@ export class BoilerParts extends Model {
   in_stock: number;
 
   @Column({ defaultValue: false })
-  bestsellers: boolean;
+  bestseller: boolean;
 
   @Column({ defaultValue: false })
   new: boolean;
